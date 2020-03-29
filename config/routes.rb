0ba6d5466c0d8498bd15db2f18ee6f4525cafe53
yokokura <yokokura0413@gmail.com>
2020-03-29 12:41:46 +0900

    resourcesを使ったルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get   'boards',         to: 'boards#index'
-  get   'boards/new',     to: 'boards#new'
-  post  'boards',         to: 'boards#create'
-  get   'boards/:id',     to: 'boards#show'
+  resources :boards
 end
