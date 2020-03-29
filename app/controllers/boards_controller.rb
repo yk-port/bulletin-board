18a021aa29fffa19e87a715c38e0a4332f4c253e
yokokura <yokokura0413@gmail.com>
2020-03-29 18:56:29 +0900

    エラーメッセージのflashの実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -10,9 +10,16 @@ class BoardsController < ApplicationController
   end
 
   def create
-    board = Board.create(board_params)
-    flash[:notice] = "「#{board.title}」の掲示板を作成しました"
-    redirect_to board
+    board = Board.new(board_params)
+    if board.save
+      flash[:notice] = "「#{board.title}」の掲示板を作成しました"
+      redirect_to board
+    else
+      redirect_to new_board_path, flash: {
+        board: board,
+        error_messages: board.errors.full_messages
+      }
+    end
   end
 
   def show
