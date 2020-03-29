45011af46efdf3975e4e19e582d2b0e44f6fa42a
yokokura <yokokura0413@gmail.com>
2020-03-29 13:46:16 +0900

    destroyアクションの作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -8,7 +8,8 @@ class BoardsController < ApplicationController
   end
 
   def create
-    Board.create(board_params)
+    board = Board.create(board_params)
+    redirect_to board
   end
 
   def show
@@ -22,8 +23,13 @@ class BoardsController < ApplicationController
   def update
     board = Board.find(params[:id])
     board.update(board_params)
-
     redirect_to board
+  end
+
+  def destroy
+    board = Board.find(params[:id])
+    board.delete
+    redirect_to boards_path
   end
 
   private
