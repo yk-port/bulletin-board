6fe8672bcfe98378e8274969917c5a030a62c928
yokokura <yokokura0413@gmail.com>
2020-03-30 22:43:58 +0900

    ログインの実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,12 @@
 class SessionsController < ApplicationController
   def create
+    user = User.find_by(name: params[:session][:name])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to mypage_path
+    else
+      render 'home/index'
+    end
   end
 
   def destroy
