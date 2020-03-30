2564a3f0eefdbb8cc5eee036b4c4f303a3c370fd
yokokura <yokokura0413@gmail.com>
2020-03-30 12:42:56 +0900

    tagをseedを使って生成

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,12 @@ if Rails.env == 'development'
   (1..50).each do |i|
     Board.create(name: "ユーザー#{i}", title: "タイトル#{i}", body: "本文#{i}")
   end
+
+  Tag.create([
+    { name: 'Ruby' },
+    { name: 'Rails' },
+    { name: 'JavaScript' },
+    { name: 'sass' },
+    { name: 'Readt' }
+  ])
 end
