155bca89ba91881787c46e40e721f95ff2941d5b
yokokura <yokokura0413@gmail.com>
2020-03-30 21:23:23 +0900

    必要なコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/me'
+
+  get 'home/index'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   root 'home#index'
   resources :boards
   resources :comments, only: %i[create destroy]
